f2199a681da19fe6d1f20e7e6d6ddd23f2b8d4df
ncpa0cpl <szymonb21@gmail.com>
2026-08-14 14:47:54 +0200

    feat: custom keymap

--- src/utils/cm-ext.ts
+++ src/utils/cm-ext.ts
@@ -1,5 +1,17 @@
 import { Compartment, Extension } from "@codemirror/state";
+import { KeyBinding, keymap } from "@codemirror/view";
 import { basicSetup, EditorView } from "codemirror";
+import {
+  standardKeymap,
+  indentMore,
+  indentLess,
+  cursorLineStart,
+  cursorLineEnd,
+  deleteLine,
+  cursorDocStart,
+  cursorDocEnd,
+} from "@codemirror/commands";
+import { foldAll, unfoldAll } from "@codemirror/language";
 
 export class CmPlugin {
   constructor(
@@ -36,6 +48,7 @@ export class CmEditor {
       doc: this.initialContent,
       root: this.shadowRoot,
       extensions: [
+        keymap.of(CmEditor.keymap()),
         basicSetup,
         EditorView.updateListener.of((u) => {
           if (u.docChanged) {
@@ -69,5 +82,74 @@ export class CmEditor {
 
   getOrAddPlugin(name: string) {
     return this.getPlugin(name) ?? this.addPlugin(name);
+  }
+
+  static keymap() {
+    const customKeymaps: KeyBinding[] = [
+      {
+        key: "Tab",
+        preventDefault: true,
+        run: indentMore,
+      },
+      {
+        key: "Shift-Tab",
+        preventDefault: true,
+        run: indentLess,
+      },
+      {
+        key: "Alt-ArrowLeft",
+        run: cursorLineStart,
+      },
+      {
+        key: "Alt-ArrowRight",
+        run: cursorLineEnd,
+      },
+      {
+        key: "Ctrl-Alt-/",
+        run: deleteLine,
+      },
+      {
+        key: "Ctrl-]",
+        run: foldAll,
+      },
+      {
+        key: "Ctrl-[",
+        run: unfoldAll,
+      },
+      {
+        key: "Home",
+        run: cursorDocStart,
+      },
+      {
+        key: "End",
+        run: cursorDocEnd,
+      },
+      {
+        key: "F2",
+        run(target) {
+          return true;
+        },
+      },
+    ];
+    const customKeymapsKeys = new Set(customKeymaps.map((e) => e.key));
+    const defaultKeymaps = standardKeymap.filter((km) => {
+      if (km.key && customKeymapsKeys.has(km.key)) {
+        return false;
+      }
+      if (km.linux && customKeymapsKeys.has(km.linux)) {
+        return false;
+      }
+      if (km.mac && customKeymapsKeys.has(km.mac)) {
+        return false;
+      }
+      if (km.win && customKeymapsKeys.has(km.win)) {
+        return false;
+      }
+      return true;
+    });
+
+    const result = [...defaultKeymaps, ...customKeymaps];
+    console.log(result);
+    return result;
   }
 }
