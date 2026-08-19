3593b97e58c88774a4efe5728a1f720d878f1e32
ncpa0cpl <szymonb21@gmail.com>
2026-08-19 12:49:53 +0000

    tab switching keybinds

--- src/modules/keymap/keymap.ts
+++ src/modules/keymap/keymap.ts
@@ -1,6 +1,14 @@
 import { MiniCodeContext } from "../../context";
 import { MinicodeKeybind, MiniCodeOptions } from "../../mini-code";
 
+function openTabCmd(idx: number): MinicodeKeybind["run"] {
+  return (ctx) => {
+    const tab = ctx.tabs.data.get().at(idx);
+    if (!tab) return;
+    ctx.tabs.focus(tab.file);
+  };
+}
+
 /**
  * Context that manages the global key shortcuts.
  *
@@ -34,6 +42,42 @@ export class KeymapContext {
           ctx.terminals.toggle();
         }
       },
+    },
+    {
+      key: "Meta-1",
+      run: openTabCmd(0),
+    },
+    {
+      key: "Meta-2",
+      run: openTabCmd(1),
+    },
+    {
+      key: "Meta-3",
+      run: openTabCmd(2),
+    },
+    {
+      key: "Meta-4",
+      run: openTabCmd(3),
+    },
+    {
+      key: "Meta-5",
+      run: openTabCmd(4),
+    },
+    {
+      key: "Meta-6",
+      run: openTabCmd(5),
+    },
+    {
+      key: "Meta-7",
+      run: openTabCmd(6),
+    },
+    {
+      key: "Meta-8",
+      run: openTabCmd(7),
+    },
+    {
+      key: "Meta-9",
+      run: openTabCmd(8),
     },
   ];
 
